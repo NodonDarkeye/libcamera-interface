d1f2fac723988029cef20031b2efcc5259ac6652
NodonDarkeye <nodondarkeye@gmail.com>
2024-01-13 12:59:57 +0100

    SETUP: initial setup.

---
+++ libcamera-interface.cpp
@@ -0,0 +1,14 @@
+#include <libcamera/libcamera.h>
+
+#include <libcamera/libcamera.h>
+
+extern "C" {
+
+// Example libcamera function
+void initializeCamera() {
+  libcamera::CameraManager manager;
+  // Perform libcamera initialization
+  // ...
+}
+
+}
